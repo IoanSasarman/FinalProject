c99fd1e24c16c9040b76b805a2633a8c99409df7
IoanSasarman <ionut_sasa@yahoo.com>
2024-11-01 16:35:54 +0200

    removed unnecessary classes and imports

--- src/main/java/com/siit/class22project/repository/ShoppingCartJPARepository.java
+++ src/main/java/com/siit/class22project/repository/ShoppingCartJPARepository.java
@@ -1,6 +1,5 @@
 package com.siit.class22project.repository;
 
-import com.siit.class22project.model.Order;
 import com.siit.class22project.model.ProductShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
